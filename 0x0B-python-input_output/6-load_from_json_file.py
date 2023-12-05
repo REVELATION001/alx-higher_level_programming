c0a3e1d12e302339d2537196c43d2f65f4e71ce0
Revelation <53403563+REVELATION001@users.noreply.github.com>
2023-12-05 15:32:20 +0100

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""load from json"""
+
+
 import json
 
 
-def load_from_json_file(filename):
-    """load from json to file"""
-    with open(filename, encoding="utf-8") as file_loaded:
-        return json.load(file+loaded)
+def from_json_string(my_str):
+    return json.loads(my_str)
